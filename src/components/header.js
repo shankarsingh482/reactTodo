e634395ca113eda6371f28ae76b95a5a3b93b334
shankar <shankar.ss482@gmail.com>
2018-11-09 15:16:44 +0530

    first commit

---
+++ src/components/header.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+class  Header extends React.Component {
+
+
+    render()
+    {   return(
+
+
+                    <h1>React Todos</h1>
+
+    )
+
+    }
+
+
+}
+
+export default Header;
+
+/*
+*   you can save both lines by
+
+export default class Header extends React.Component
+*
+* */
+
